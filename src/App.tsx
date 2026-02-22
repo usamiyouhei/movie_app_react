5cba0ca5998bd07bea417d51cfc25cdd0222ecd1
youhei usami <usao9110@icloud.com>
2026-02-22 14:06:11 +0400

    2/22

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import "./App.css";
+
 function App() {
-  return <div>Hello World</div>;
+  alert("Hello World");
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
