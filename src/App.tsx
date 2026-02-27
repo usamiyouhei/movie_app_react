bb80532a8343a4e75b3ead74587aab700dcce468
youhei usami <usao9110@icloud.com>
2026-02-27 18:20:39 +0400

    2/27

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+type Movie = {
+  id: number;
+  original_title: string;
+  poster_path: string;
+  overview: string;
+};
+
+type MovieJson = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
 function App() {
   // const apiKey = import.meta.env.ViTE_TMDB_API_KEY;
   const defaultMovieList = [
@@ -47,36 +70,49 @@ function App() {
   ];
 
   const [keyword, setKeyword] = useState("");
+  const [movieList, setMovieList] = useState<Movie[]>([]);
 
   const fetchMovieList = async () => {
-    const response = await fetch(
-      "https://api.themoviedb.org/3/movie/popular?language=ja-US&page=1",
-      {
-        headers: {
-          Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
-        },
+    let url = "";
+    if (keyword) {
+      url = `https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=false&language=ja-US&page=1`;
+    } else {
+      url = "https://api.themoviedb.org/3/movie/popular?language=ja-US&page=1";
+    }
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
       },
-    );
+    });
 
     const data = await response.json();
-    console.log(data);
+    setMovieList(
+      data.results.map((movie: MovieJson) => ({
+        id: movie.id,
+        original_title: movie.original_title,
+        overview: movie.overview,
+        poster_path: movie.poster_path,
+      })),
+    );
   };
 
   useEffect(() => {
     fetchMovieList();
-  }, []);
+  }, [keyword]);
 
   return (
     <div>
       <div>{keyword}</div>
       <input type="text" onChange={(e) => setKeyword(e.target.value)} />
       {/* <button onClick={() => alert("hello")}>click</button> */}
-      {defaultMovieList
-        .filter((movie) => movie.name.includes(keyword))
+      {movieList
+        .filter((movie) => movie.original_title.includes(keyword))
         .map((movie) => (
           <div key={movie.id}>
-            <h2>{movie.name}</h2>
-            <img src={movie.image} />
+            <h2>{movie.original_title}</h2>
+            <img
+              src={`https://media.themoviedb.org/t/p/w600_and_h900_face/${movie.poster_path}`}
+            />
             <p>{movie.overview}</p>
           </div>
         ))}
